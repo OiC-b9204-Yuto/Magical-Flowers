4544f129ad70d50924f1874e6867b5baf543e853
OiC-b9204-Yuto <71886577+OiC-b9204-Yuto@users.noreply.github.com>
2022-03-10 04:16:41 +0900

    ステージ生成（二次元配列からの固定生成）作成 ステージの情報取得用クラスの作成

---
+++ MagicalFlowers/Assets/Project/Program/Scripts/Stage/StageManager.cs
@@ -0,0 +1,50 @@
+﻿using MagicalFlowers.Base;
+using MagicalFlowers.Common;
+using MagicalFlowers.Item;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MagicalFlowers.Stage
+{
+    public class StageManager : SingletonMonoBehaviour<StageManager>
+    {
+        StageData stageData;
+        [SerializeField] StageGenerator stageGenerator;
+
+        [SerializeField] Vector2Int getTilePos;
+        void Start()
+        {
+            stageData = StageUtility.CreateTestStageData();
+            stageGenerator.Generate(stageData);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                DebugLogger.Log(GetItemData(getTilePos.x, getTilePos.y) + ":" + GetActorData(getTilePos.x, getTilePos.y));
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                DebugLogger.Log($"{getTilePos}:{CheckMove(getTilePos.x, getTilePos.y)}");
+            }
+        }
+
+        public bool CheckMove(int x, int y)
+        {
+            return stageData.map[y, x] == 0 && stageData.actors.Find(n => n.Position.x == x && n.Position.y == y) == null;
+        }
+        
+        public ItemObject GetItemData(int x,int y)
+        {
+            return  stageData.items.Find(n => n.Position.x == x && n.Position.y == y);
+        }
+
+        public BaseActor GetActorData(int x, int y)
+        {
+            return stageData.actors.Find(n => n.Position.x == x && n.Position.y == y);
+        }
+    }
+}
---
+++ MagicalFlowers/Assets/Project/Program/Scripts/Stage/StageUtility.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicalFlowers.Stage
+{
+    public static class StageUtility
+    {
+        public static StageData CreateTestStageData()
+        {
+            StageData data = new StageData(
+                new int[10,10]
+                {
+                    {1,1,1,1,1,1,1,1,1,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,0,0,0,0,0,0,0,0,1 },
+                    {1,1,1,1,1,1,1,1,1,1 }
+                }
+                );
+            return data; 
+        }
+    }
+}
