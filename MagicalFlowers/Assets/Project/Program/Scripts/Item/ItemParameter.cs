061bf9be3ed60c926329b98bbdabaecfc43b4a2c
OiC-b9336-Naoki <b9336@oic.jp>
2022-03-10 02:06:49 +0900

    itemParameter置き、ScriptableObjectを用いた実装

--- MagicalFlowers/Assets/Project/Program/Scripts/Item/ItemParameter.cs
+++ MagicalFlowers/Assets/Project/Program/Scripts/Item/ItemParameter.cs
@@ -1,12 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace MagicalFlowers.Item
 {
-
+    [CreateAssetMenu(menuName = "ScriptableObject/Create ItemData")]
     public class ItemParameter : ScriptableObject
     {
+        [SerializeReference] private data _data;
 
+        [MenuItem("CONTEXT/ItemParameter/Set Equipment")]
+        public static void SetEquipment(MenuCommand command)
+        {
+            var itemParameter = (ItemParameter) command.context;
+            itemParameter._data = new equipment();
+            EditorUtility.SetDirty(itemParameter);
+        }
+        [MenuItem("CONTEXT/ItemParameter/Set Available")]
+        public static void SetAvailable(MenuCommand command)
+        {
+            var itemParameter = (ItemParameter)command.context;
+            itemParameter._data = new Available();
+            EditorUtility.SetDirty(itemParameter);
+        }
+        [MenuItem("CONTEXT/ItemParameter/Set Flower")]
+        public static void SetFlower(MenuCommand command)
+        {
+            var itemParameter = (ItemParameter)command.context;
+            itemParameter._data = new Flower();
+            EditorUtility.SetDirty(itemParameter);
+        }
     }
+
+    [Serializable]
+    public abstract class data
+    {
+        public string Name;
+        [Multiline]public string Description;
+    }
+    [Serializable]
+    public class equipment : data
+    {
+        public int ID;
+        public int Atk;
+        public int Def;
+    }
+    [Serializable]
+    public class Available : data
+    {
+        public StateType stateType;
+        public int value;
+    }
+    [Serializable]
+    public class Flower : data
+    {
+        public int equipmentID;
+    }
+
+    public enum StateType { MaxHealth,Health,Attack,Defense};
 }
