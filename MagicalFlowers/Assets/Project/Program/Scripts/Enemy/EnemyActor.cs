278d8411299fac1c1f2a13b5b7743a640310c40d
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2022-03-10 16:41:22 +0900

    敵にとりあえず、ランダムで動くように実装 / プレイヤーが近くに居たらプレイヤーの方に移動するスクリプトを実装中

--- MagicalFlowers/Assets/Project/Program/Scripts/Enemy/EnemyActor.cs
+++ MagicalFlowers/Assets/Project/Program/Scripts/Enemy/EnemyActor.cs
@@ -1,4 +1,5 @@
 using MagicalFlowers.Base;
+using MagicalFlowers.Stage;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,31 +8,60 @@ namespace MagicalFlowers.Enemy
 {
     public class EnemyActor : BaseActor
     {
+        //プレイヤーの情報
+        Vector2Int PlayerPosition;
+        //移動先座標
+        public Vector3 targetPosition;
+        [SerializeField] Vector2Int ShownMyPosition;
+        [SerializeField] Vector2Int ShownTargetPosition;
+        [SerializeField]int RandomX;
+        [SerializeField] int RandomY;
+
+
+        void Start()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            position = StageUtility.WorldPos2StagePos(transform.position);
+        }
+        void Update()
+        {
+
+        }
+
         protected override void ActionBeginProcess()
         {
-            throw new System.NotImplementedException();
+            var step = 5 * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+            if (transform.position == targetPosition)
+            {
+                ActorState = ActorStateType.ActionEnd;
+            }
         }
 
         protected override void ActionEndProcess()
         {
-            throw new System.NotImplementedException();
+            position += direction;
         }
 
         protected override void InputWaitProcess()
         {
-            throw new System.NotImplementedException();
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            ShownMyPosition = position;
+            ShownTargetPosition = PlayerPosition;
+            RandomX = Random.Range(-10, 10);
+            RandomY = Random.Range(-10, 10);
+            if (RandomX >= 3){ RandomX = 1; }else if(RandomX <= -3) { RandomX = -1; }else if(RandomX <= 0) { RandomX = 0; }
+            if (RandomY >= 3){ RandomY = 1; }else if(RandomY <= -3) { RandomY = -1; }else if (RandomY <= 0) { RandomY = 0; }
+            direction = new Vector2Int(RandomX, RandomY);
+            if (StageManager.Instance.CheckMove(position, direction))
+            {
+                targetPosition = transform.position + new Vector3(direction.x, 0, -direction.y);
+                ActorState = ActorStateType.ActionBegin;
+            }
+            
         }
     }
 }
