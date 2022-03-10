1580f2e63c3a8ec9cc25d5ca1626d9f007e59fe6
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2022-03-10 17:52:39 +0900

    移動方向を向くシステムを実装 / プレイヤーとの距離が近くなったらという処理を実装

--- MagicalFlowers/Assets/Project/Program/Scripts/Enemy/EnemyActor.cs
+++ MagicalFlowers/Assets/Project/Program/Scripts/Enemy/EnemyActor.cs
@@ -1,4 +1,5 @@
 using MagicalFlowers.Base;
+using MagicalFlowers.Player;
 using MagicalFlowers.Stage;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,15 +10,15 @@ namespace MagicalFlowers.Enemy
     public class EnemyActor : BaseActor
     {
         //プレイヤーの情報
-        Vector2Int PlayerPosition;
+        [SerializeField]private PlayerActor Player;
+        [SerializeField] Vector2Int PlayerPosition;
         //移動先座標
         public Vector3 targetPosition;
-        [SerializeField] Vector2Int ShownMyPosition;
-        [SerializeField] Vector2Int ShownTargetPosition;
-        [SerializeField]int RandomX;
+        [SerializeField] Vector3 LatestPositon;
+        [SerializeField] int RandomX;
         [SerializeField] int RandomY;
-
-
+        [SerializeField] bool LockPlayer;
+        [SerializeField] float Distance;
         void Start()
         {
             Initialize();
@@ -25,13 +26,9 @@ namespace MagicalFlowers.Enemy
 
         void Initialize()
         {
+            Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerActor>();
             position = StageUtility.WorldPos2StagePos(transform.position);
         }
-        void Update()
-        {
-
-        }
-
         protected override void ActionBeginProcess()
         {
             var step = 5 * Time.deltaTime;
@@ -49,13 +46,28 @@ namespace MagicalFlowers.Enemy
 
         protected override void InputWaitProcess()
         {
-            ShownMyPosition = position;
-            ShownTargetPosition = PlayerPosition;
-            RandomX = Random.Range(-10, 10);
-            RandomY = Random.Range(-10, 10);
-            if (RandomX >= 3){ RandomX = 1; }else if(RandomX <= -3) { RandomX = -1; }else if(RandomX <= 0) { RandomX = 0; }
-            if (RandomY >= 3){ RandomY = 1; }else if(RandomY <= -3) { RandomY = -1; }else if (RandomY <= 0) { RandomY = 0; }
-            direction = new Vector2Int(RandomX, RandomY);
+            PlayerPosition = Player.Position;
+            Distance = (PlayerPosition - position).sqrMagnitude;
+            Mathf.Abs(Distance);
+            if (Distance > 15) //プレイヤーとの距離が遠い時
+            {
+                Vector3 diff = this.transform.position - LatestPositon;
+                LatestPositon = this.transform.position;
+                if(diff.magnitude > 0.01f)
+                {
+                    transform.rotation = Quaternion.LookRotation(diff);
+                }
+                RandomX = Random.Range(-1, 2);
+                RandomY = Random.Range(-1, 2);
+                direction = new Vector2Int(RandomX, RandomY);
+            }
+            else //プレイヤーとの距離が近い時
+            {
+                this.transform.LookAt(Player.transform);
+                RandomX = Random.Range(-1, 2);
+                RandomY = Random.Range(-1, 2);
+                direction = new Vector2Int(RandomX, RandomY);
+            }
             if (StageManager.Instance.CheckMove(position, direction))
             {
                 targetPosition = transform.position + new Vector3(direction.x, 0, -direction.y);
