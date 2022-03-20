36bc571d60d5cee1c6322275b1e61ede7d8bacdd
OiC-b9204-Yuto <71886577+OiC-b9204-Yuto@users.noreply.github.com>
2022-03-21 03:15:57 +0900

    アイテムデータの場所変更 インベントリのナビゲーションバグ修正

--- MagicalFlowers/Assets/Project/Program/Scripts/Player/PlayerInventory.cs
+++ MagicalFlowers/Assets/Project/Program/Scripts/Player/PlayerInventory.cs
@@ -21,11 +21,25 @@ namespace MagicalFlowers.Player
         public void ItemUse(int index)
         {
             //enumで装備や使用アイテムの分岐
+            switch (inventory[index]._data.Type)
+            {
+                case ItemType.Equipment:
+
+                    break;
+                case ItemType.Available:
+
+                    break;
+                case ItemType.Flower:
+
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void ItemThrow(int index)
         {
-
+            
         }
 
         public void ItemDrop(int index)
